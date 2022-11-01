9d29ff9328d7536f5e7ea2fa6638c96109ad98cc
ant52ho <87661562+ant52ho@users.noreply.github.com>
2022-11-01 00:08:17 -0400

    doc viewer and word replacing

---
+++ frontend/src/components/ViewDoc.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import DocViewer from "react-doc-viewer";
+import "./ViewDoc.css";
+import FileViewer from "react-file-viewer";
+
+function ViewDoc() {
+  const docs = [
+    { uri: "https://docxtemplater.com/tag-example.docx" },
+    // { uri: "https://url-to-my-pdf.pdf" },
+    // { uri: require("./example-files/pdf.pdf") }, // Local File
+  ];
+
+  const file = "https://docxtemplater.com/tag-example.docx";
+  const type = "docx";
+
+  return (
+    <div className="fileViewer">
+      <FileViewer fileType={type} filePath={file} />;
+    </div>
+  );
+}
+
+export default ViewDoc;
